7606777b25f3d92dde6f61dbb9adcdef2acb8431
Michelle Torres <lic.nancy.torres@gmail.com>
2013-02-27 23:11:48 -0600

    Exercises 1 and 2

--- Ejercicios 2013A/tablas_estruct.php
+++ Ejercicios 2013A/tablas_estruct.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * @package Ejercicios2013A
+ * @license @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @author Michelle Torres <lic.nancy.torres@gmail.com>
+ * @version 1.0
+ */
+ 
 function tablas(){
 	for($tabla = 1 ; $tabla <= 12 ; $tabla++ ){
  		for($mul = 1 ; $mul <= 12 ; $mul++ ){
--- Ejercicios 2013A/tablas_poo.php
+++ Ejercicios 2013A/tablas_poo.php
@@ -1,9 +1,19 @@
  <?php
+ /**
+ * @package Ejercicios2013A
+ * @license @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @author Michelle Torres <lic.nancy.torres@gmail.com>
+ * @version 1.0
+ */
  
  class Tablas{
  	public $inicio;
 	public $fin;
 	
+	/**
+	 * @param int $i Indica el inicio de la tabla
+	 * @param int $f Indica el fin de la tabla
+	 */
 	function __construct($i,$f){
 		$this->inicio = $i;
 		$this->fin = $f;
@@ -18,14 +28,22 @@
  		}
  	}
  }
-	/* Obtener valores por GET para indicar el
-	 * inicio y fin de las tablas
-	 * i = inicio
-	 * f = fin
-	 * 
-	 * Validar los rangos de las tablas
-	 */ 
+ 
+ 
+/* Obtener valores por GET para indicar el
+ * inicio y fin de las tablas
+ * i = inicio
+ * f = fin
+ * 
+ * Validar los rangos de las tablas
+ */ 
+
+//Para debuggear utilizo print_r y muestro el valor de REQUEST
+//que tiene dentro POST, GET y COOKIES	 
 print_r($_REQUEST);
+
+
+//Si no mandan valores, setear por default a 1 y 12
  if ( isset($_REQUEST['ini']) )
  	$inicio = $_REQUEST['ini'];
  else {
@@ -38,6 +56,7 @@ print_r($_REQUEST);
     $fin = 12;
  }
  
+//Validar que se pueda realizar la tabla
  if($inicio > $fin)
  	echo 'no puedo hacer las tablas';
  else{
